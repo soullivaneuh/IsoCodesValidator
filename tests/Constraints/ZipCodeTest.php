ed872e99e080b48659bab740ff33f778cb78304e
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-04-22 13:36:19 +0200

    Add ZipCodeTest

---
+++ tests/Constraints/ZipCodeTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace SLLH\IsoCodesValidator\Constraints;
+
+class ZipCodeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider getValidCountries
+     */
+    public function testValidCountries($country)
+    {
+        $zipCode = new ZipCode(array('country' => $country));
+
+        $this->assertSame($country, $zipCode->country);
+    }
+
+    public function getValidCountries()
+    {
+        return array(
+            array('Canada'),
+            array('France'),
+            array('Netherlands'),
+            array('US'),
+        );
+    }
+
+    /**
+     * @dataProvider getInvalidCountries
+     * @expectedException Symfony\Component\Validator\Exception\ConstraintDefinitionException
+     */
+    public function testInvalidCountries($country)
+    {
+        $zipCode = new ZipCode(array('country' => $country));
+    }
+
+    public function getInvalidCountries()
+    {
+        return array(
+            array('canada'),
+            array('CANADA'),
+            array('caNada'),
+            array('france'),
+            array('FRANCE'),
+            array('frAnce'),
+            array('netherlands'),
+            array('NETHERLANDS'),
+            array('neTherlands'),
+            array('us'),
+            array('uS'),
+            array('Us'),
+            array('qwertyuiop')
+        );
+    }
+}
