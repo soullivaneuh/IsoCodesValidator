d6192ebbf887415a89cafa8bf3a9f0e4549c3742
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-06-23 00:38:50 +0200

    New Isbn validator

---
+++ src/Constraints/Isbn.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace SLLH\IsoCodesValidator\Constraints;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Exception\ConstraintDefinitionException;
+
+/**
+ * @Annotation
+ * @Target({"PROPERTY", "METHOD", "ANNOTATION"})
+ *
+ * @author Sullivan Senechal <soullivaneuh@gmail.com>
+ */
+class Isbn extends Constraint
+{
+    public $message = 'This value is not a valid ISBN.';
+    public $type = null;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct($options = null)
+    {
+        parent::__construct($options);
+
+        if ($this->type !== null && !in_array($this->type, [10, 13])) {
+            throw new ConstraintDefinitionException('The option "type" must be 10 or 13');
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getDefaultOption()
+    {
+        return 'type';
+    }
+}
