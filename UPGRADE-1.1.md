ff8416707bb9e8da8821059f14803f466ec0caa9
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-06-23 14:52:33 +0200

    Refactor ZipCode validator options

--- UPGRADE-1.1.md
+++ UPGRADE-1.1.md
@@ -28,3 +28,14 @@ class CustomValidator extends AbstractIsoCodesConstraintValidator
     // ...
 }
 ```
+
+## ZipCode validator country option
+
+ZipCode validator was refactored to get benefit of
+[new country code patterns](https://github.com/ronanguilloux/IsoCodes/blob/1.2.0/src/IsoCodes/ZipCode.php#L27-L203).
+
+Because of that, `Canada`, `France` and `Netherlands` country options are now deprecated.
+
+Use country codes equivalent instead: `CA`, `FR` and `NL`.
+
+The `US` option still the same, but the related constant will be removed in 2.0.
--- src/Constraints/ZipCodeValidator.php
+++ src/Constraints/ZipCodeValidator.php
@@ -25,8 +25,8 @@ class ZipCodeValidator extends AbstractIsoCodesConstraintValidator
 
         if ($constraint->country == ZipCode::ALL) {
             $validated = false;
-            foreach (ZipCode::$countries as $country) {
-                if ($this->validateCountry($value, $country)) {
+            foreach (IsoCodes\ZipCode::getAvailableCountries() as $country) {
+                if (IsoCodes\ZipCode::validate($value, $country)) {
                     $validated = true;
                     break;
                 }
@@ -34,28 +34,8 @@ class ZipCodeValidator extends AbstractIsoCodesConstraintValidator
             if ($validated === false) {
                 $this->createViolation($constraint->message);
             }
-        } elseif (!$this->validateCountry($value, $constraint->country)) {
+        } elseif (!IsoCodes\ZipCode::validate($value, $constraint->country)) {
             $this->createViolation($constraint->message);
         }
-    }
-
-    /**
-     * @deprecated To be removed when bumping requirements to ronanguilloux/isocodes ~1.2
-     *
-     * @param mixed  $value
-     * @param string $country
-     *
-     * @return bool
-     */
-    private function validateCountry($value, $country)
-    {
-        $deprecatedOptionsBridge = [
-            'US'            => 'US',
-            'Canada'        => 'CA',
-            'France'        => 'FR',
-            'Netherlands'   => 'NL',
-        ];
-
-        return IsoCodes\ZipCode::validate($value, $deprecatedOptionsBridge[$country]);
     }
 }
