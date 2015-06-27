f795e7e116fe4e29d0c57b3bc8957a1d44351a84
Sullivan SENECHAL <soullivaneuh@gmail.com>
2015-06-27 16:33:55 +0200

    Symfony and Silex internal integration

---
+++ tests/Bundle/DependencyInjection/Compiler/TranslationPassTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace SLLH\IsoCodesValidator\Tests\Bundle\DependencyInjection\Compiler;
+
+use Matthias\SymfonyDependencyInjectionTest\PhpUnit\AbstractCompilerPassTestCase;
+use SLLH\IsoCodesValidator\Bundle\DependencyInjection\Compiler\TranslationPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class TranslationPassTest extends AbstractCompilerPassTestCase
+{
+    protected function registerCompilerPass(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new TranslationPass());
+    }
+
+    public function testWithTranslatorService()
+    {
+        $this->registerService('translator.default', 'Symfony\Bundle\FrameworkBundle\Translation\Translator');
+        $this->compile();
+
+        // TODO: Check if translations resources are correctly added to the translator service.
+    }
+}
