4f6fde267700335d5f95eac4b4e92882db502dc4
Sullivan SENECHAL <soullivaneuh@gmail.com>
2016-04-16 18:27:00 +0200

    Use IsoCodes test data for validators
    This will reduce a lot the test code length and make new integrations easier.

--- tests/Constraints/CifValidatorTest.php
+++ tests/Constraints/CifValidatorTest.php
@@ -10,40 +10,6 @@ final class CifValidatorTest extends AbstractGenericConstraintValidatorTest
     /**
      * {@inheritdoc}
      */
-    public function getValidValues()
-    {
-        return [
-            ['N0032484H'],
-            ['A09212275'],
-            ['A59032557'],
-            ['A17031246'],
-            ['B85358596'],
-            ['E61685095'],
-            ['V27236942'],
-            ['G61685095'],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function getInvalidValues()
-    {
-        return [
-            ['K11111111'],
-            ['L61685095'],
-            ['X61685095'],
-            ['Y61685095'],
-            ['N0032484'],
-            ['N0032484I'],
-            ['M0032484I'],
-            ['M0032484H'],
-        ];
-    }
-
-    /**
-     * {@inheritdoc}
-     */
     protected function getInvalidMessage()
     {
         return 'This value is not a valid CIF.';
